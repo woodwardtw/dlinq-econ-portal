fddc6c1989b991db1b59ac13b440ff0bcc913000
woodwardtw <twwoodward@me.com>
2021-09-13 15:23:59 -0400

    resource item work, css improved

--- inc/acf.php
+++ inc/acf.php
@@ -8,6 +8,38 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+
+//resources
+function resource_image(){
+    if(get_field('featured_image')){
+        $img_data = get_field('featured_image');
+        $url = $img_data['url'];
+        $alt = $img_data['alt'];
+        return "<img src='{$url}' alt='{$alt}' class='resource_image'>";
+    } else {
+        $img_url = get_stylesheet_directory() . '/imgs/network.png';
+        return "<img src='' alt='Random image meant to convey the idea of data.' class='resource-image'>";
+    }
+}
+
+function econ_details($field){
+    if(get_field($field)){
+        $html = '';
+        $resources = get_field($field);
+        //var_dump($resources);
+        foreach ($resources as $key => $resource) {
+            // code...
+            $name = $resource->name;
+            $html .= "<div class='resource-item'>{$name}</div>";
+        }
+        return $html;
+    } else {
+        return 'Not found.';
+    }
+}
+
+
+
     //save acf json
         add_filter('acf/settings/save_json', 'econ_portal_json_save_point');
          
--- loop-templates/content-single-resource.php
+++ loop-templates/content-single-resource.php
@@ -13,21 +13,33 @@ defined( 'ABSPATH' ) || exit;
 
 	<header class="entry-header">
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-
-			
-
-		</div><!-- .entry-meta -->
+		<a href="<?php the_field('resource_link')?>"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
 	<div class="entry-content">
 
-		<?php the_content(); ?>
+		<div class="row">
+			<div class='col-md-4 image-holder'>
+				<?php echo resource_image();?>
+			</div>
+			<div class="col-md-8 description">
+				<?php the_field('resource_description')?>
+				<a class="btn btn-primary" href="<?php the_field('resource_link');?>">Go to Resource</a>
+			</div>
+			<div class="col-md-4 course resource-detail">
+				<h2>Course</h2>
+				<?php echo econ_details('course');?>
+			</div>
+			<div class="col-md-4 objective resource-detail">
+				<h2>Objective</h2>
+				<?php echo econ_details('objective');?>
+			</div>
+			<div class="col-md-4 software resource-detail">
+				<h2>Software</h2>
+				<?php echo econ_details('software');?>
+			</div>
+		</div>
 
 		<?php
 		wp_link_pages(
