310586d515520913193fb6a6e7b6bc150dcdc6e2
woodwardtw <twwoodward@me.com>
2021-09-13 16:15:59 -0400

    resource page work

--- loop-templates/content-single-resource.php
+++ loop-templates/content-single-resource.php
@@ -27,16 +27,21 @@ defined( 'ABSPATH' ) || exit;
 				<?php the_field('resource_description')?>
 				<a class="btn btn-primary" href="<?php the_field('resource_link');?>">Go to Resource</a>
 			</div>
+		</div>
+		<div class="resource-detail-section row">
+			<div class="col-md-12">
+				<h2>Details</h2>
+			</div>
 			<div class="col-md-4 course resource-detail">
-				<h2>Course</h2>
+				<h3>Course</h3>
 				<?php echo econ_details('course');?>
 			</div>
 			<div class="col-md-4 objective resource-detail">
-				<h2>Objective</h2>
+				<h3>Objective</h3>
 				<?php echo econ_details('objective');?>
 			</div>
 			<div class="col-md-4 software resource-detail">
-				<h2>Software</h2>
+				<h3>Software</h3>
 				<?php echo econ_details('software');?>
 			</div>
 		</div>
