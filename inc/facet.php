d0c57a6468e5b6ac9e0cbbc40e60b448db716e41
woodwardtw <twwoodward@me.com>
2021-09-14 10:09:15 -0400

    hover facet

--- inc/facet.php
+++ inc/facet.php
@@ -11,7 +11,9 @@ defined( 'ABSPATH' ) || exit;
 while ( have_posts() ): the_post(); ?>
     <div class="col-md-6">
         <div class="card">
-          <?php echo resource_image();?>
+            <a href="<?php the_permalink(); ?>">
+                <?php echo resource_image();?>
+            </a>
           <div class="card-body">
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>        
           </div>
